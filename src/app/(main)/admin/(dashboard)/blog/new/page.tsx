c478e06c462e6351da8291c5bff7551ed923ccc6
Treasure56 <131236913+Treasure56@users.noreply.github.com>
2024-03-11 11:45:37 +0100

    implemented api��

--- src/app/(main)/admin/(dashboard)/blog/new/page.tsx
+++ src/app/(main)/admin/(dashboard)/blog/new/page.tsx
@@ -3,17 +3,62 @@
 import AppInput, { AppInputProps } from "@/components/ui/AppInput";
 import FormButton from "@/components/ui/FormButton";
 import { z } from "zod";
-import React from "react";
+import React, { useState } from "react";
+import AdminAuth from "@/components/AdminAuth";
+import FormMessage from "@/components/ui/FormMessage";
+import useFormSubmit from "@/hooks/useFormSubmit";
+import { API } from "@/utils/constants";
+import { getCookie } from "@/utils/functions/cookies";
+import { ApiFormMessage } from "@/utils/types/basicTypes";
 
 export default function Page() {
+  const [errors, setErrors] = useState([true]);
+  const {
+    formProps: { onSubmit, key },
+    formState,
+    setErrorMessage,
+    setSuccessMessage,
+    reset,
+  } = useFormSubmit<ApiFormMessage>({
+    url: `${API}admin/addblog`,
+    headers: {
+      token: getCookie("token") ?? "",
+    },
+    onComplete(data) {
+      if (!data.message || !data) return setErrorMessage("An error occurred");
+      if (data.message === "blog header uploaded") {
+        reset();
+        return setSuccessMessage("story uploaded successfully");
+      }
+      setErrorMessage(data.message);
+    },
+  });
   return (
     <div className="flex flex-col gap-2 p-3 md:p-4 max-w-[500px] mx-auto rounded md:border w-full mt-4  bg-light shadow">
       <h1 className="r-text-xl r-font-bold">New Blog title</h1>
-      <form className="flex flex-col gap-3 py-3">
-        {formFields.map((item) => {
-          return <AppInput key={item.name} {...item} />;
+      <form onSubmit={onSubmit} key={key} className="flex flex-col gap-3 py-3">
+        <FormMessage {...formState} />
+        <AdminAuth />
+        {formFields.map((item, i) => {
+          return (
+            <AppInput
+              key={item.name}
+              {...item}
+              onErrorChange={(hasError) => {
+                setErrors((prev) =>
+                  prev.map((error, index) => (index === i ? hasError : error))
+                );
+              }}
+            />
+          );
         })}
-        <FormButton className="btn-primary">Submit</FormButton>
+        <FormButton
+          loading={formState.loading}
+          disabled={errors.includes(true)}
+          className="btn-primary"
+        >
+          Submit
+        </FormButton>
       </form>
     </div>
   );
